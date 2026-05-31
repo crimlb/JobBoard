e236014977544df27de8f6740b81f0708d9fead0
Cristina Buffone <crimlb87@gmail.com>
2026-06-01 00:10:18 +0200

    fix cors

--- Backend/index.js
+++ Backend/index.js
@@ -30,7 +30,11 @@ app.use(helmet());
 // Cors controlla quali origini esterne possono chiamare le nostre API.
 // Da commentare/decommentare in base a quando il frontend è attivo.
 app.use(cors({
-    origin: 'http://localhost:5173', // URL esatto del frontend 
+    origin: [
+        'http://localhost:5173',
+        'https://jobboard-git-main-cristina-dev.vercel.app',
+        'https://jobboard-9r4fo31zk-cristina-dev.vercel.app'
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
